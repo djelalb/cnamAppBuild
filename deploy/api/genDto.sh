d1a212a1ca0c52f3e205eb9187ec4a1d85dc427e
Djelal BOUDJI <djelal2004@gmail.com>
2025-01-22 19:24:44 +0100

    chore: fix files

--- deploy/api/genDto.sh
+++
@@ -1,3 +0,0 @@
-php vendor/bin/doctrine orm:convert-mapping --namespace="" --force --from-database yml ./api/config/yaml
-php vendor/bin/doctrine orm:generate-entities --generate-annotations=false --update-entities=true --generate-methods=false ./api/src
-composer update
